c4facca478845fe09f22eabf1f84d477e5a45533
zhongph <izph@foxmail.com>
2022-04-06 22:59:04 +0800

    feat: auto-complete function

---
+++ src/Auto-Complete/interface.ts
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react';
+import { InputProps } from '../Input/interface';
+
+export interface DataSourceObject {
+  value: string;
+}
+
+export type DataSourceType<T = {}> = T & DataSourceObject;
+
+export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
+  /** 搜索补全项的时候调用 */
+  onSearch: (str: string) => DataSourceType[] | Promise<DataSourceType[]>;
+  /** 选中下拉选项时触发的回调 */
+  onSelect?: (item: DataSourceType) => void;
+  /** 自定义渲染下拉选项，返回 ReactElement */
+  renderOption?: (item: DataSourceType) => ReactElement;
+}
