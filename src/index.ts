a8307338011805fdd2e915676ee9d3e62949473e
zhongph <izph@foxmail.com>
2022-04-06 21:36:01 +0800

    feat: add input function

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,4 @@ export { default as Button } from './Button';
 export { default as Transition } from './Transition';
 export { default as Menu } from './Menu';
 export { default as Icon } from './Icon';
+export { default as Input } from './Input';
