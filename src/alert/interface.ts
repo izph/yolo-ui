91f7ce3e0b3d59b9a5cd0b6bd07b8d4150b9aa82
zhongph <izph@foxmail.com>
2022-05-04 18:44:21 +0800

    fix: fix up

--- src/alert/interface.ts
+++ src/alert/interface.ts
@@ -3,20 +3,37 @@ import React, { ReactNode } from 'react';
 export type AlertType = 'info' | 'success' | 'error' | 'warning';
 
 export interface AlertProps {
-  /** 警告提示内容 */
+  /**
+   * 警告提示内容
+   */
   message: string;
-  /** 警告内容描述 */
+  /**
+   * 警告内容描述
+   */
   description?: string;
-  /** 警告的类型, defaultValue: info */
+  /**
+   * 警告的类型
+   * defaultValue: info
+   */
   type?: AlertType;
-  /** 是否可关闭 */
+  /**
+   * 是否可关闭
+   */
   closable?: boolean;
-  /** 是否显示图标 */
+  /**
+   * 是否显示图标
+   */
   showIcon?: boolean;
-  /** 自定义图标 */
+  /**
+   * 自定义图标
+   */
   icon?: ReactNode;
-  /** 类名 */
+  /**
+   * 用户自定义的类名
+   */
   className?: string;
-  /** 关闭的回调 */
+  /**
+   * 关闭的回调
+   */
   onClose?: (e: React.MouseEvent<HTMLElement, MouseEvent>) => void;
 }
